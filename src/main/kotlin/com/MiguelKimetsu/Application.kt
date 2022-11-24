67e0d91ea9b6f67e2456205f3fb2ce66ddfc7908
MiguelCM19 <lesothosaurio@gmail.com>
2022-11-24 15:54:28 +0100

    Base Project with dependencies

---
+++ src/main/kotlin/com/MiguelKimetsu/Application.kt
@@ -0,0 +1,16 @@
+package com.MiguelKimetsu
+
+import io.ktor.server.application.*
+import com.MiguelKimetsu.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
+}
